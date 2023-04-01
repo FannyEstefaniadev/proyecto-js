1f551a0999dbc98e21c32e9ec7e746d092bb8eef
FannyEstefaniadev <fannyestefania8@gmail.com>
2023-04-01 11:42:45 -0300

    inicio-desplegable

---
+++ index.js
@@ -0,0 +1,38 @@
+const menu = document.querySelector('.hamburguesa');
+const navegacion = document.querySelector('.navegacion');
+const overlay = document.createElement('div');
+
+document.addEventListener('DOMContentLoaded', () => {
+  menu.addEventListener('click', toggleMenu);
+});
+
+const toggleMenu = () => {
+  overlay.classList.remove('ocultar-background');
+  overlay.classList.add('pantalla-completa');
+  const body = document.querySelector('body');
+  body.appendChild(overlay);
+
+  if (navegacion.classList.contains('ocultar')) {
+    navegacion.classList.remove('ocultar');
+  } else {
+    navegacion.classList.add('ocultar');
+  } botoncerrar();
+
+};
+
+const botoncerrar = () => {
+  const btnCerrar = document.createElement('p');
+  btnCerrar.textContent = 'x';
+  btnCerrar.classList.add('btn-cerrar');
+  navegacion.appendChild(btnCerrar);
+  cerrarMenu(btnCerrar);
+  overlay.remove;
+
+}
+
+const cerrarMenu = (boton) => {
+  boton.addEventListener('click', () => {
+    navegacion.classList.add('ocultar');
+    overlay.classList.add('ocultar-background');
+  })
+}
